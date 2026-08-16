4d31386811443fe0908ec235adaf0c7398d7ac1e
raptus <fake@mail.com>
2026-08-16 18:38:45 +0200

    removed non vanilla files

--- lisp/multiple-cursors.el
+++
@@ -1,6 +0,0 @@
-(use-package multiple-cursors
-  :ensure t
-  :bind (("C-M-<down>" . mc/mark-next-like-this)
-         ("C-M-<up>" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)))
-
