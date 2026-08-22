066e94e00731d5f7db2df933c17cdb88c4bf8215
raptus <fake@mail.com>
2026-08-22 22:18:32 +0200

    a few changes

--- init.el
+++ init.el
@@ -1,12 +1,33 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;; -*- lexical-binding: t; -*-
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((Lexical-binding . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; nobody needs those
+(setq-default make-backup-files nil)
+(setq-default auto-save-default nil)
+
+;; theming
+(load-theme 'wombat t)
+
+(custom-set-faces
+ '(tab-bar ((t (:inherit header-line))))
+ '(tab-bar-tab ((t (:inherit header-line :weight bold :underline t))))
+ '(tab-bar-tab-inactive ((t (:inherit header-line-inactive)))))
+
+
+;; imports
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
-
-;(load "lisp/multiple-cursors")
-;(load "python-stuff")
 
 (require 'mortal-keymap)
 
@@ -15,10 +36,11 @@
   "A minor mode for mortal-related keybindings."
   :lighter " Mortal"
   :keymap mortal-map
-)
+  :global t
+  )
 
-(add-hook 'prog-mode-hook #'mortal-mode)
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(mortal-mode)
+;; (add-hook 'prog-mode-hook #'mortal-mode)
 
 
 ;; cursor style
@@ -29,13 +51,30 @@
 
 ;; ui modes
 (tab-bar-mode 1)
-(desktop-save-mode 1)
+(menu-bar-mode -1)
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+
+;; save sessions
+(desktop-save-mode 1)
 
 
 ;; editing modes
 (delete-selection-mode 1)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq-local python-indent-offset 4)
 
-;; nobody needs those
-(setq-default make-backup-files nil)
-(setq-default auto-save-default nil)
+(add-hook 'python-ts-mode-hook
+          (lambda ()
+            (setq-local python-indent-offset 4)
+            (setq-local indent-tabs-mode nil)
+            (setq-local tab-width 4)
+            (add-hook 'before-save-hook
+                      #'delete-trailing-whitespace nil t)))
+
+(add-to-list 'major-mode-remap-alist
+             '(python-mode . python-ts-mode))
--- lisp/mortal-keymap.el
+++ lisp/mortal-keymap.el
@@ -1,33 +1,85 @@
-;; -*- lexical-binding: t; -*-
+;; -*- Lexical-binding: t; -*-
 
-(defun mortal/save-and-close-file ()
-  "Save the current buffer, and close the window."
+(defun mortal/quit ()
+  "Quit the current operation or exit the minibuffer."
   (interactive)
-  (when (buffer-file-name)
-    (save-buffer))
-  (delete-window))
+  (if (minibufferp)
+      (minibuffer-keyboard-quit)
+    (keyboard-quit)))
 
-(defun mortal/new-file-buffer ()
-  "Open a new tab containing a fresh, unsaved buffer.
-The buffer is not associated with any file on disk; you will
-be prompted for a filename the first time you save it."
+
+(defun mortal/new-tab-with-empty-buffer ()
+  "Create a new tab-bar tab and switch it to a fresh empty buffer."
   (interactive)
   (tab-bar-new-tab)
-  (let ((buffer (generate-new-buffer "untitled")))
-    (switch-to-buffer buffer)
-    (setq buffer-offer-save t)
-    buffer))
+  (switch-to-buffer (generate-new-buffer "*new*"))
+  (text-mode))
 
 
-(defun mortal/copy-line-or-region ()
-  "Copy the active region to the kill ring, or the whole current
-line if no region is active. Point is moved to the start of the
-line afterward."
+
+(defun mortal/select-word-right ()
+  "Start the region if none is active, then extend it forward using `mortal/forward-word'."
   (interactive)
-  (if (use-region-p)
-      (copy-region-as-kill (region-beginning) (region-end))
-    (copy-region-as-kill (line-beginning-position) (line-end-position)))
-  (beginning-of-line))
+  (unless (use-region-p)
+    (push-mark (point) t t))
+  (mortal/forward-word))
+
+(defun mortal/select-word-left ()
+  "Start the region if none is active, then extend it backward using `mortal/backward-word'."
+  (interactive)
+  (unless (use-region-p)
+    (push-mark (point) t t))
+  (mortal/backward-word))
+
+
+(defun mortal/select-paragraph-up ()
+  "Start the region if none is active, then extend it backward by one paragraph."
+  (interactive)
+  (unless (use-region-p)
+    (push-mark (point) t t))
+  (backward-paragraph 1))
+
+(defun mortal/select-paragraph-down ()
+  "Start the region if none is active, then extend it forward by one paragraph."
+  (interactive)
+  (unless (use-region-p)
+    (push-mark (point) t t))
+  (forward-paragraph 1))
+
+
+(defun mortal/select-char-left ()
+  "Start the region if none is active, then extend it backward by one char."
+  (interactive)
+  (unless (use-region-p)
+    (push-mark (point) t t))
+  (backward-char 1))
+
+(defun mortal/select-char-right ()
+  "Start the region if none is active, then extend it forward by one char."
+  (interactive)
+  (unless (use-region-p)
+    (push-mark (point) t t))
+  (forward-char 1))
+
+
+(defun mortal/select-line-up ()
+  "Start the region if none is active, then extend it up by one line."
+  (interactive)
+  (unless (use-region-p)
+    (push-mark (point) t t))
+  (let ((col (current-column)))
+    (forward-line -1)
+    (move-to-column col)))
+
+(defun mortal/select-line-down ()
+  "Start the region if none is active, then extend it down by one line."
+  (interactive)
+  (unless (use-region-p)
+    (push-mark (point) t t))
+  (let ((col (current-column)))
+    (forward-line 1)
+    (move-to-column col)))
+
 
 
 (defun mortal/forward-word ()
@@ -55,6 +107,63 @@ and stop there without moving into the preceding word."
         (backward-char 1)
         (skip-chars-backward " \t\r"))
     (forward-same-syntax -1)))
+
+
+
+(defvar mortal/selection-commands
+  '(mortal/select-word-right
+    mortal/select-word-left
+    mortal/select-char-right
+    mortal/select-char-left
+    mark-whole-buffer
+    mortal/select-line-up
+    mortal/select-line-down
+    mortal/select-paragraph-up
+    mortal/select-paragraph-down
+    mortal/move-line-up
+    mortal/move-line-down)
+  "Commands that are allowed to extend the region without clearing it.")
+
+(defun mortal/move-line-up ()
+  "Move the current line up.
+If a region is active, move all marked lines up instead."
+  (interactive)
+  (if (use-region-p)
+      (mortal/move-lines-vertically -1)
+    (progn
+      (transpose-lines 1)
+      (forward-line -2))))
+
+(defun mortal/move-line-down ()
+  "Move the current line down.
+If a region is active, move all marked lines down instead."
+  (interactive)
+  (if (use-region-p)
+      (mortal/move-lines-vertically 1)
+    (progn
+      (forward-line 1)
+      (transpose-lines 1)
+      (forward-line -1))))
+
+
+
+(defun mortal/save-and-close-file ()
+  "Save the current buffer, and close the window."
+  (interactive)
+  (when (buffer-file-name)
+    (save-buffer))
+  (delete-window))
+
+(defun mortal/copy-line-or-region ()
+  "Copy the active region to the kill ring, or the whole current
+line if no region is active. Point is moved to the start of the
+line afterward."
+  (interactive)
+  (if (use-region-p)
+      (copy-region-as-kill (region-beginning) (region-end))
+    (copy-region-as-kill (line-beginning-position) (line-end-position)))
+  (beginning-of-line))
+
 
 
 (defun mortal/backward-delete-word ()
@@ -138,7 +247,7 @@ search string with its contents; otherwise start isearch normally."
         (deactivate-mark)
         (isearch-forward nil 1)
         (isearch-yank-string string))
-    (isearch-forward)))
+    (isearch-complete)))
 
 (defun mortal/query-replace-or-yank-region ()
   "Run `query-replace'. If a region is active, use its contents as
@@ -153,101 +262,6 @@ start `query-replace' normally."
         (query-replace from to))
     (call-interactively #'query-replace)))
 
-
-(defvar mortal/selection-commands
-  '(mortal/select-word-right
-    mortal/select-word-left
-    mortal/select-char-right
-    mortal/select-char-left
-    mark-whole-buffer
-    mortal/select-line-up
-    mortal/select-line-down
-    mortal/select-paragraph-up
-    mortal/select-paragraph-down
-    mortal/move-line-up
-    mortal/move-line-down)
-  "Commands that are allowed to extend the region without clearing it.")
-
-(defun mortal/move-line-up ()
-  "Move the current line up.
-If a region is active, move all marked lines up instead."
-  (interactive)
-  (if (use-region-p)
-      (mortal/move-lines-vertically -1)
-    (progn
-      (transpose-lines 1)
-      (forward-line -2))))
-
-(defun mortal/move-line-down ()
-  "Move the current line down.
-If a region is active, move all marked lines down instead."
-  (interactive)
-  (if (use-region-p)
-      (mortal/move-lines-vertically 1)
-    (progn
-      (forward-line 1)
-      (transpose-lines 1)
-      (forward-line -1))))
-
-(defun mortal/select-word-right ()
-  "Start the region if none is active, then extend it forward using `mortal/forward-word'."
-  (interactive)
-  (unless (use-region-p)
-    (push-mark (point) t t))
-  (mortal/forward-word))
-
-(defun mortal/select-word-left ()
-  "Start the region if none is active, then extend it backward using `mortal/backward-word'."
-  (interactive)
-  (unless (use-region-p)
-    (push-mark (point) t t))
-  (mortal/backward-word))
-
-(defun mortal/select-char-right ()
-  "Start the region if none is active, then extend it forward by one char."
-  (interactive)
-  (unless (use-region-p)
-    (push-mark (point) t t))
-  (forward-char 1))
-
-(defun mortal/select-char-left ()
-  "Start the region if none is active, then extend it backward by one char."
-  (interactive)
-  (unless (use-region-p)
-    (push-mark (point) t t))
-  (backward-char 1))
-
-(defun mortal/select-line-down ()
-  "Start the region if none is active, then extend it down by one line."
-  (interactive)
-  (unless (use-region-p)
-    (push-mark (point) t t))
-  (let ((col (current-column)))
-    (forward-line 1)
-    (move-to-column col)))
-
-(defun mortal/select-line-up ()
-  "Start the region if none is active, then extend it up by one line."
-  (interactive)
-  (unless (use-region-p)
-    (push-mark (point) t t))
-  (let ((col (current-column)))
-    (forward-line -1)
-    (move-to-column col)))
-
-(defun mortal/select-paragraph-down ()
-  "Start the region if none is active, then extend it forward by one paragraph."
-  (interactive)
-  (unless (use-region-p)
-    (push-mark (point) t t))
-  (forward-paragraph 1))
-
-(defun mortal/select-paragraph-up ()
-  "Start the region if none is active, then extend it backward by one paragraph."
-  (interactive)
-  (unless (use-region-p)
-    (push-mark (point) t t))
-  (backward-paragraph 1))
 
 (defun mortal/deactivate-mark-unless-selecting ()
   "Deactivate the region unless the just-run command was one that
@@ -264,105 +278,224 @@ extends selection, or a mouse-drag/mouse-selection command."
 
 
 
+(defun mortal/copy-line-down ()
+  "Duplicate the current line, placing the copy below and moving point there."
+  (interactive)
+  (let ((col (current-column))
+        (line (buffer-substring (line-beginning-position) (line-end-position))))
+    (end-of-line)
+    (newline)
+    (insert line)
+    (move-to-column col)))
+
+(defun mortal/copy-line-up ()
+  "Duplicate the current line, placing the copy above and moving point there."
+  (interactive)
+  (let ((col (current-column))
+        (line (buffer-substring (line-beginning-position) (line-end-position))))
+    (beginning-of-line)
+    (insert line "\n")
+    (forward-line -1)
+    (move-to-column col)))
+
+(defun mortal/close-current-tab ()
+  "Close the current tab. If it's the last tab, delete the frame instead."
+  (interactive)
+  (if (> (length (tab-bar-tabs)) 1)
+      (tab-bar-close-tab)
+    (delete-frame)))
+
+
+
 (defvar mortal-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "<escape>") (kbd "C-g"))
+    ;; undefine C-*, M-*, and C-M-* besides M-x, C-i, C-j, C-m.
+    (define-key map (kbd "C-a") #'undefined)
+    (define-key map (kbd "C-b") #'undefined)
+    (define-key map (kbd "C-c") #'undefined)
+    (define-key map (kbd "C-d") #'undefined)
+    (define-key map (kbd "C-e") #'undefined)
+    (define-key map (kbd "C-f") #'undefined)
+    (define-key map (kbd "C-g") #'undefined)
+    (define-key map (kbd "C-h") #'undefined)
+    (define-key map (kbd "C-k") #'undefined)
+    (define-key map (kbd "C-l") #'undefined)
+    (define-key map (kbd "C-n") #'undefined)
+    (define-key map (kbd "C-o") #'undefined)
+    (define-key map (kbd "C-p") #'undefined)
+    (define-key map (kbd "C-q") #'undefined)
+    (define-key map (kbd "C-r") #'undefined)
+    (define-key map (kbd "C-s") #'undefined)
+    (define-key map (kbd "C-t") #'undefined)
+    (define-key map (kbd "C-u") #'undefined)
+    (define-key map (kbd "C-v") #'undefined)
+    (define-key map (kbd "C-w") #'undefined)
+    (define-key map (kbd "C-x") #'undefined)
+    (define-key map (kbd "C-y") #'undefined)
+    (define-key map (kbd "C-z") #'undefined)
 
+    (define-key map (kbd "M-a") #'undefined)
+    (define-key map (kbd "M-b") #'undefined)
+    (define-key map (kbd "M-c") #'undefined)
+    (define-key map (kbd "M-d") #'undefined)
+    (define-key map (kbd "M-e") #'undefined)
+    (define-key map (kbd "M-f") #'undefined)
+    (define-key map (kbd "M-g") #'undefined)
+    (define-key map (kbd "M-h") #'undefined)
+    (define-key map (kbd "M-i") #'undefined)
+    (define-key map (kbd "M-j") #'undefined)
+    (define-key map (kbd "M-k") #'undefined)
+    (define-key map (kbd "M-l") #'undefined)
+    (define-key map (kbd "M-m") #'undefined)
+    (define-key map (kbd "M-n") #'undefined)
+    (define-key map (kbd "M-o") #'undefined)
+    (define-key map (kbd "M-p") #'undefined)
+    (define-key map (kbd "M-q") #'undefined)
+    (define-key map (kbd "M-r") #'undefined)
+    (define-key map (kbd "M-s") #'undefined)
+    (define-key map (kbd "M-t") #'undefined)
+    (define-key map (kbd "M-u") #'undefined)
+    (define-key map (kbd "M-v") #'undefined)
+    (define-key map (kbd "M-w") #'undefined)
+    (define-key map (kbd "M-y") #'undefined)
+    (define-key map (kbd "M-z") #'undefined)
+
+    (define-key map (kbd "C-M-a") #'undefined)
+    (define-key map (kbd "C-M-b") #'undefined)
+    (define-key map (kbd "C-M-c") #'undefined)
+    (define-key map (kbd "C-M-d") #'undefined)
+    (define-key map (kbd "C-M-e") #'undefined)
+    (define-key map (kbd "C-M-f") #'undefined)
+    (define-key map (kbd "C-M-g") #'undefined)
+    (define-key map (kbd "C-M-h") #'undefined)
+    (define-key map (kbd "C-M-i") #'undefined)
+    (define-key map (kbd "C-M-j") #'undefined)
+    (define-key map (kbd "C-M-k") #'undefined)
+    (define-key map (kbd "C-M-l") #'undefined)
+    (define-key map (kbd "C-M-m") #'undefined)
+    (define-key map (kbd "C-M-n") #'undefined)
+    (define-key map (kbd "C-M-o") #'undefined)
+    (define-key map (kbd "C-M-p") #'undefined)
+    (define-key map (kbd "C-M-q") #'undefined)
+    (define-key map (kbd "C-M-r") #'undefined)
+    (define-key map (kbd "C-M-s") #'undefined)
+    (define-key map (kbd "C-M-t") #'undefined)
+    (define-key map (kbd "C-M-u") #'undefined)
+    (define-key map (kbd "C-M-v") #'undefined)
+    (define-key map (kbd "C-M-w") #'undefined)
+    (define-key map (kbd "C-M-x") #'undefined)
+    (define-key map (kbd "C-M-y") #'undefined)
+    (define-key map (kbd "C-M-z") #'undefined)
+
+    
+    (define-key map (kbd "<escape>") #'mortal/quit)
+    
     ;; emacs prefixes
     (define-key map (kbd "<f1>") ctl-x-map)
     (define-key map (kbd "<f3>") ctl-x-r-map)
-    (define-key map (kbd "<f5>") 'help-command)
+    (define-key map (kbd "<f5>") #'help-command)
 
     ;; window management
-    (define-key map (kbd "<f2> 1") 'delete-other-windows)
-    (define-key map (kbd "<f2> 2") 'split-window-right)
-    (define-key map (kbd "<f2> 3") 'split-window-below)
+    (define-key map (kbd "<f2> 1") #'delete-other-windows)
+    (define-key map (kbd "<f2> 2") #'split-window-right)
+    (define-key map (kbd "<f2> 3") #'split-window-below)
     
-    (define-key map (kbd "<f2> <left>") 'windmove-left)
-    (define-key map (kbd "<f2> <right>") 'windmove-right)
-    (define-key map (kbd "<f2> <up>") 'windmove-up)
-    (define-key map (kbd "<f2> <down>") 'windmove-down)
+    (define-key map (kbd "<f2> <left>") #'windmove-left)
+    (define-key map (kbd "<f2> <right>") #'windmove-right)
+    (define-key map (kbd "<f2> <up>") #'windmove-up)
+    (define-key map (kbd "<f2> <down>") #'windmove-down)
 
     ;; tab management 
     (dotimes (i 9)
       (let ((n (1+ i)))
-	(define-key map (kbd (format "C-%d" n))
-		    (lambda () (interactive) (tab-bar-select-tab n)))))
-
+        (define-key map
+                    (kbd (format "C-%d" n))
+                    (eval `(lambda ()
+                             (interactive)
+                             (tab-bar-select-tab ,n))))))
+    
+    (define-key map (kbd "C-S-n") #'mortal/new-tab-with-empty-buffer)
+    (define-key map (kbd "C-w") #'mortal/close-current-tab)
+    
     ;; select
-    (define-key map (kbd "C-a") 'mark-whole-buffer)
+    (define-key map (kbd "C-a") #'mark-whole-buffer)
     
     (define-key map (kbd "C-S-<left>") #'mortal/select-word-left)
     (define-key map (kbd "C-S-<right>") #'mortal/select-word-right)
-
     (define-key map (kbd "C-S-<up>") #'mortal/select-paragraph-up)
     (define-key map (kbd "C-S-<down>") #'mortal/select-paragraph-down)
     
-    (define-key map (kbd "S-<right>") #'mortal/select-char-right)
     (define-key map (kbd "S-<left>") #'mortal/select-char-left)
+    (define-key map (kbd "S-<right>") #'mortal/select-char-right)
 
     (define-key map (kbd "S-<up>") #'mortal/select-line-up)
     (define-key map (kbd "S-<down>") #'mortal/select-line-down)
 
 
     ;; movement
-    (define-key map (kbd "C-<left>") 'mortal/backward-word)
-    (define-key map (kbd "C-<right>") 'mortal/forward-word)
+    (define-key map (kbd "C-<right>") #'mortal/forward-word)
+    (define-key map (kbd "C-<left>") #'mortal/backward-word)
     
-    (define-key map (kbd "C-w") 'goto-line)
+    (define-key map (kbd "C-g") #'goto-line)
     
     ;; evil mode
-    (define-key map (kbd "M-h") 'backward-char)
-    (define-key map (kbd "M-j") 'next-line)
-    (define-key map (kbd "M-k") 'previous-line)
-    (define-key map (kbd "M-l") 'forward-char)
+    (define-key map (kbd "M-h") #'backward-char)
+    (define-key map (kbd "M-j") #'next-line)
+    (define-key map (kbd "M-k") #'previous-line)
+    (define-key map (kbd "M-l") #'forward-char)
 
-    (define-key map (kbd "M-C-h") 'mortal/backward-word)
-    (define-key map (kbd "M-C-l") 'mortal/forward-word)
-    
+    (define-key map (kbd "M-C-l") #'mortal/forward-word)
+    (define-key map (kbd "M-C-h") #'mortal/backward-word)
+    (define-key map (kbd "M-C-j") #'forward-paragraph)
+    (define-key map (kbd "M-C-k") #'backward-paragraph)
 
     ;; emacs movement
-    (define-key map (kbd "M-p") 'previous-line)
-    (define-key map (kbd "M-n") 'next-line)
-    (define-key map (kbd "M-f") 'forward-char)
-    (define-key map (kbd "M-b") 'backward-char)
+    (define-key map (kbd "M-p") #'previous-line)
+    (define-key map (kbd "M-n") #'next-line)
+    (define-key map (kbd "M-f") #'forward-char)
+    (define-key map (kbd "M-b") #'backward-char)
 
-    (define-key map (kbd "M-C-f") 'mortal/backward-word)
-    (define-key map (kbd "M-C-b") 'mortal/forward-word)
+    (define-key map (kbd "M-C-f") #'mortal/forward-word)
+    (define-key map (kbd "M-C-b") #'mortal/backward-word)
+    (define-key map (kbd "M-C-n") #'forward-paragraph)
+    (define-key map (kbd "M-C-p") #'backward-paragraph)
+
 
     ;; move line
-    (define-key map (kbd "M-<up>") 'mortal/move-line-up)    
-    (define-key map (kbd "M-<down>") 'mortal/move-line-down)    
+    (define-key map (kbd "M-<up>") #'mortal/move-line-up)    
+    (define-key map (kbd "M-<down>") #'mortal/move-line-down)
+
+    ;; copy line
+    (define-key map (kbd "C-M-S-<down>") #'mortal/copy-line-down)    
+    (define-key map (kbd "C-M-S-<up>") #'mortal/copy-line-up)    
 
     ;; deleting
-    (define-key map (kbd "C-<delete>") 'mortal/forward-delete-word)
-    (define-key map (kbd "C-<backspace>") 'mortal/backward-delete-word)
-    (define-key map (kbd "C-S-k") 'mortal/delete-line)
+    (define-key map (kbd "C-<delete>") #'mortal/forward-delete-word)
+    (define-key map (kbd "C-<backspace>") #'mortal/backward-delete-word)
+    (define-key map (kbd "C-S-k") #'mortal/delete-line)
 
     ;; clipboard
-    (define-key map (kbd "C-x") 'mortal/kill-line-or-region)
-    (define-key map (kbd "C-c") 'mortal/copy-line-or-region)
-    (define-key map (kbd "C-v") 'yank)
+    (define-key map (kbd "C-x") #'mortal/kill-line-or-region)
+    (define-key map (kbd "C-c") #'mortal/copy-line-or-region)
+    (define-key map (kbd "C-v") #'yank)
 
     ;; file actions
-    (define-key map (kbd "C-s") 'save-buffer)
-    (define-key map (kbd "C-w") 'mortal/save-and-close-file)
-    (define-key map (kbd "C-n") 'mortal/new-file-buffer)
-    (define-key map (kbd "C-o") 'dired)
+    (define-key map (kbd "C-s") #'save-buffer)
+    (define-key map (kbd "C-w") #'mortal/save-and-close-file)
+    (define-key map (kbd "C-n") #'mortal/new-file-buffer)
+    (define-key map (kbd "C-o") #'find-file)
 
     ;; undo / redo
-    (define-key map (kbd "C-z") 'undo)
-    (define-key map (kbd "C-y") 'redo)
-    (define-key map (kbd "C-S-z") 'redo)
+    (define-key map (kbd "C-z") #'undo)
+    (define-key map (kbd "C-y") #'undo-redo)
 
     ;; insert new line
-    (define-key map (kbd "C-<return>") 'mortal/insert-line-below)
-    (define-key map (kbd "C-S-<return>") 'mortal/insert-line-above)
+    (define-key map (kbd "C-<return>") #'mortal/insert-line-below)
+    (define-key map (kbd "C-S-<return>") #'mortal/insert-line-above)
 
     ;; search / replace
-    ; add directory search
-    (define-key map (kbd "C-f") 'mortal/isearch-forward-or-yank-region)
-    (define-key map (kbd "C-r") 'mortal/query-replace-or-yank-region)
+    (define-key map (kbd "C-f") #'mortal/isearch-forward-or-yank-region)
+    (define-key map (kbd "C-r") #'mortal/query-replace-or-yank-region)
     
     map))
 
